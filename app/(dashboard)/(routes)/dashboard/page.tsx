7820e692b777af9970f8b54c1fde9f8fb540b434
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-01 17:31:23 +0530

    chore : protected routes only when user get authorized

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return (
+        <div className='text-2xl text-gray-950 leading-10'>
+            <h1>Dashboard (Protected user) </h1>
+        </div>
+    )
+}
