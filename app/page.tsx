b7d8b2e8d60bee96f76412801477b550fff69457
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-01 17:30:36 +0530

    chore : moved to protected routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <div className='text-2xl text-gray-950 leading-10'>
-      <h1>Hello World</h1>
-    </div>
-  )
-}
