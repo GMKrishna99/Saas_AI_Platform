a20a3a59e17f348e3ac94eb5a937bdb6c97e5c54
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-02 14:14:10 +0530

    feat : pathname added for active hover when user click

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,10 @@
 "use client"
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Logo from "@/public/logo.png"
 import { Montserrat } from "next/font/google";
+
 
 
 import { cn } from "@/lib/utils";
@@ -57,6 +59,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white ">
             <div className="px-3 py-2 flex-1">
@@ -79,6 +82,7 @@ const Sidebar = () => {
                             key={route.href}
                             className={cn(
                                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
 
                             )}>
                             <div className="flex items-center flex-1">
