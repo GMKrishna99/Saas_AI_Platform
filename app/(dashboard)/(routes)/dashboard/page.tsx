da75d7f1e5327811d9192b0bfd49bed8271ce2f2
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-08-02 10:22:57 +0530

    feat: user profile button to sign-out

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
     return (
         <div className='text-2xl text-gray-950 leading-10'>
             <h1>Dashboard (Protected user) </h1>
+            <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
